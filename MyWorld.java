991c0e927a37616e86576be3c27882271978cd4a
348586678 <348586678@gapps.yrdsb.ca>
2025-04-25 13:42:31 -0400

    Added a snake to project

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,9 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Snake green_snake = new Snake();
+        addObject(green_snake, 200, 300);
+        
     }
 }
